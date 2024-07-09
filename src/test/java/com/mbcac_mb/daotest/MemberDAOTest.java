72647ab4ede45e79a7ac99f6bdd9464fa8515ccf
djadudwns12 <djadudwns12@naver.com>
2024-07-09 12:07:05 +0900

    test : log4jdbc 테스트

--- src/test/java/com/mbcac_mb/daotest/MemberDAOTest.java
+++ src/test/java/com/mbcac_mb/daotest/MemberDAOTest.java
@@ -29,24 +29,24 @@ public class MemberDAOTest
 //		System.out.println(mdao.getTime()); 
 //	}
 
-//	@Test
-//	public void testInsertMember() {
-//		MemberVO m =
-//		MemberVO.builder()
-//		.userid("douner")
-//		.userPwd("1234")
-//		.userName("도우너")
-////		.mobile("010-0000-1111")
-//		.email("douner@amamma.com")
-//		.build();
-//		
-//		if(mdao.registerMember(m)==1) {
-//			System.out.println("회원저장성공");
-//		}
+	@Test
+	public void testInsertMember() {
+		MemberVO m =
+		MemberVO.builder()
+		.userid("douner")
+		.userPwd("1234")
+		.userName("도우너")
+//		.mobile("010-0000-1111")
+		.email("douner@amamma.com")
+		.build();
+		
+		if(mdao.registerMember(m)==1) {
+			System.out.println("회원저장성공");
+		}
 		
 		
 //		System.out.println(mdao.registerMember(m)); 
-//	}
+	}
 	
 	
 //	@Test
